5ae8b575b5390318ef6b568f22e83d47230a52c5
LoQeN00 <LoQeN00@o2.pl>
2022-09-15 18:51:01 +0200

    Added box shadow to footer

--- components/BlogPostsList.tsx
+++ components/BlogPostsList.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const BlogPostsList = ({ posts }: Props) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 mb-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 mb-10 flex-1">
       {posts.map((post) => {
         return (
           <BlogPostListItem
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import Header from '../components/Header';
 import { InferGetStaticPropsType } from 'next';
 import { apolloClient } from '../graphql/apolloClient';
 import { GetAllPostsListDocument, GetAllPostsListQuery, GetAllPostsListQueryVariables } from '../generated/graphql';
+import Footer from '../components/Footer';
 
 const HomePage = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl mx-auto flex justify-center flex-col">
       <Header />
       <BlogPostsList posts={data.posts} />
+      <Footer />
     </div>
   );
 };
