cf96f37aa20660d796ceb19a582730448ab1d1f6
LoQeN00 <LoQeN00@o2.pl>
2022-09-15 18:31:26 +0200

    Added Stars component

--- components/Stars.tsx
+++ components/Stars.tsx
@@ -6,11 +6,15 @@ type Props = {
 
 const Stars = ({ rating }: Props) => {
   const progressBar = (rating / 5) * 100 + '%';
+  console.log(progressBar);
 
   return (
     <div className="relative inline-block before:content-['★★★★★'] before:text-2xl before:text-[#ccc]">
       <div
-        className={`absolute top-0 left-0 whitespace-nowrap overflow-hidden w-[${progressBar}] before:text-2xl before:content-['★★★★★'] before:text-[#f8ce0b]`}
+        className={`absolute top-0 left-0 whitespace-nowrap overflow-hidden before:text-2xl before:content-['★★★★★'] before:text-[#f8ce0b]`}
+        style={{
+          width: progressBar,
+        }}
       ></div>
     </div>
   );
